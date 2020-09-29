f40b4a61ce55b8ecfbfcaf06cd6681476a0dae69
Ilya <51325702+treger78@users.noreply.github.com>
2020-09-29 14:33:32 +0300

    feat: make reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+
+    let nString = '';
+    let nArr = [];
+
+    nString = String(n);
+
+    for (var j = 0, i = nString.length - 1; i >= 0; i--, j++) {
+      nArr[i] = nString[j];      
+    }
+
+    nString = nArr.toString();
+
+    for (var i = 0; i < nString.length; i++) {
+      nString = nString.replace(',', '');      
+    }
+    
+    n = parseInt(nString);
+
+    return n;
+
 }
